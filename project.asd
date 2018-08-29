cb52f0bc9ffd8d9f4798af9158d1bf692e532a45
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-08-29 21:49:57 +0200

    it doesn't load
    Component "project/addons" not found, required by
       [Condition of type ASDF/FIND-COMPONENT:MISSING-DEPENDENCY]

--- project.asd
+++ project.asd
@@ -3,4 +3,4 @@
   :version "0.0.1"
   :class :package-inferred-system
   :defsystem-depends-on ("asdf-package-system")
-  :depends-on ("project/anddons"))
+  :depends-on ("project/addons"))
--- run.lisp
+++ run.lisp
@@ -1,1 +1,5 @@
+;; link the folder containing project.asd to
+;; ~/.local/share/common-lisp/source/<folder>
+
+;; ln -s  /home/martin/stage/cl-try-asdf3 ~/.local/share/common-lisp/source
 (asdf:load-system "project")
